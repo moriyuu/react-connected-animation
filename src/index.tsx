176ac4b73b64dc63a4e0c16b2caa89abb4e16897
moriyuu <moriyu54321@gmail.com>
2018-12-24 17:59:42 +0900

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class App extends React.Component {
+  render() {
+    return <div>init</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
