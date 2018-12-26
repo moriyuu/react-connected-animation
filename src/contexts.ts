9f70b6b458e5afc1ec3b21755b22df23a29e3524
moriyuu <moriyu54321@gmail.com>
2018-12-26 12:21:50 +0900

    remove all

--- src/contexts.ts
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { useConnectedAnimation } from "./useConnectedAnimation";
-
-export const PositionContext = React.createContext({} as ReturnType<
-  typeof useConnectedAnimation
->);
